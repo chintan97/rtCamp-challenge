c7c06a9a9197c7ab84761720b56fb9c4fd1ac7a2
Chintan Patel <chintan97patel@gmail.com>
2017-08-17 11:16:31 +0530

    Update search.php

--- search.php
+++ search.php
@@ -1,9 +1,10 @@
 <?php
-// Follower search requests are processed here. Maximum 20 hints are shown.
+// Follower search requests are processed here. Maximum 10 hints are shown.
 
 if(isset($_GET['q']))
 {
 session_start();
+require_once('libs/TwitterAPIExchange.php');
 $un = $_SESSION['user_name'];
 $xmlDoc = new DOMDocument();
 $xmlDoc->load("$un.xml");
@@ -16,20 +17,48 @@ if (strlen($q) > 0) {
 
     foreach ($books as $book) {
         $name = $book->getElementsByTagName("name1")->item(0)->nodeValue;
+        $handle = $book->getElementsByTagName("handle1")->item(0)->nodeValue;
         $id = $book->getElementsByTagName("id1")->item(0)->nodeValue;
 		$temp = preg_replace("/[^a-zA-Z]/", "", $name);
 		$temp1 = preg_replace("/[^a-zA-Z]/", "", $id);
 
-		if (  (strpos ( strtolower ( $temp ), $q ) !== false or strpos ( strtolower ( $temp1 ), $q ) !== false) and $count <= 20 ){
+		if (  (strpos ( strtolower ( $temp ), $q ) !== false or strpos ( strtolower ( $temp1 ), $q ) !== false) and $count <= 10 ){
 			
 			// Hints are shown here.
-			echo "<a href='#' onClick=ajaxLoad('$id')>$name</a><br />";
+			echo "<a href='#' onClick=ajaxLoad('$id')>$name, Handle: @$handle</a><br />";
 			$count++;
 		}
     }
 		
+	if ($count < 10)
+	{
+		$url = "https://api.twitter.com/1.1/users/search.json";
+		$requestMethod = "GET";
+		
+		$settings = array(
+		'oauth_access_token' => $_SESSION['oauth_token1'],
+		'oauth_access_token_secret' => $_SESSION['oauth_token_secret1'],
+		'consumer_key' => "MQT5hHDMRPOlcaqgLsEvJtF0Y",
+		'consumer_secret' => "3zUAzwdJmMmFdkhRPwl07CXkNZQRAa53aEHsRPGYsR6IWrLoUi"
+		);
+		
+		$temp_c = 10-$count;
+		$getfield = "?q=$q&count=$temp_c";
+		$twitter = new TwitterAPIExchange($settings);
+		$string1 = json_decode($twitter->setGetfield($getfield)
+		->buildOauth($url, $requestMethod)
+		->performRequest(),$assoc = TRUE);
+		
+		foreach($string1 as $items)
+		{
+			?>
+			<a href='#' onClick=ajaxLoad('<?php echo $items['id_str'] ?>')><?php echo $items['name'] ?>, Handle: @<?php echo $items['screen_name'] ?></a><br />
+			<?php
+			$count++;
+		}
+	}
 	if ($count == 1){
-		echo "No follower matched!!!";
+		echo "No follower/ public account matched!!!";
 	}
 
 }
